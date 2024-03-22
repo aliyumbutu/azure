51e9c7e8bd18cc90f8a5b40afe11a112216742d0
Aliyu Muhammad Butu <39166994+aliyumbutu@users.noreply.github.com>
2024-03-22 17:23:22 +0100

    Update tasks-api.py

--- tasks-api.py
+++ tasks-api.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, jsonify
+#!/usr/bin/python3
 import json
 import os
 import pymysql
-
-app = Flask(__name__)
 
 # Database connection parameters - update as needed
 DB_USER = os.getenv("DB_USER") or "LAMP2"
@@ -20,70 +18,134 @@ db = pymysql.connect(
 )
 cursor = db.cursor()
 
+
 # Create a new task
-@app.route('/tasks', methods=['POST'])
-def create_task():
-    data = request.get_json()
-    title = data.get('title')
+def create_task(title):
     try:
         cursor.execute("INSERT INTO tasks (title) VALUES (%s)", title)
         db.commit()
         cursor.execute("SELECT MAX(id) AS id FROM tasks")
         row = cursor.fetchone()
         resp = get_task(row["id"])
-        return jsonify(resp[0]), 201
+        return (resp[0], 201)
     except Exception as e:
-        return str(e), 500
+        return (str(e), 500)
+
 
 # Get all tasks
-@app.route('/tasks', methods=['GET'])
 def get_tasks():
     try:
         cursor.execute(
             "SELECT id, title, date_format(created, '%Y-%m-%d %H:%i') as created FROM tasks"
         )
-        return jsonify(cursor.fetchall()), 200
+        return (cursor.fetchall(), 200)
     except Exception as e:
-        return str(e), 500
+        return (str(e), 500)
+
 
 # Get an individual task
-@app.route('/tasks/<int:id>', methods=['GET'])
 def get_task(id):
     try:
         cursor.execute(
             "SELECT id, title, date_format(created, '%Y-%m-%d %H:%i') as created \
-										FROM tasks WHERE id=%s", id
+										FROM tasks WHERE id="
+            + str(id)
         )
         row = cursor.fetchone()
         return (row if row is not None else "", 200 if row is not None else 404)
     except Exception as e:
-        return str(e), 404
+        return ("", 404)
+
 
 # Update an existing task
-@app.route('/tasks/<int:id>', methods=['PUT'])
-def update_task(id):
-    data = request.get_json()
-    title = data.get('title')
+def update_task(id, title):
     try:
         cursor.execute("UPDATE tasks SET title=%s WHERE id=%s", (title, id))
         db.commit()
-        return jsonify(get_task(id)), 200
+        return get_task(id)
     except Exception as e:
-        return str(e), 500
+        return (str(e), 500)
+
 
 # Delete an existing task
-@app.route('/tasks/<int:id>', methods=['DELETE'])
 def delete_task(id):
     try:
         resp = get_task(id)
         if resp[1] == 200:
             cursor.execute("DELETE FROM tasks WHERE id=%s", id)
             db.commit()
-            return "", 200
+            return ("", 200)
         else:
-            return jsonify(resp), resp[1]
+            return resp
     except Exception as e:
-        return str(e), 500
+        return (str(e), 500)
 
-if __name__ == '__main__':
-    app.run()
+
+# Returns the HTTP request method
+def get_method():
+    return os.getenv("REQUEST_METHOD") or "GET"
+
+
+# Returns the query string
+def get_query_string():
+    query_string = os.getenv("QUERY_STRING") or ""
+    return query_string.replace("%20", " ").replace("%2F", "/").replace("+", " ")
+
+
+# Returns the task ID if set in the request query string
+def get_task_id():
+    query_string = get_query_string()
+    qs_parts = query_string.split("/")
+    return qs_parts[0] if qs_parts[0].isnumeric() else None
+
+
+# Returns the task title from the query string if set
+def get_task_title():
+    title = None
+    query_string = get_query_string()
+    if query_string != "":
+        qs_parts = query_string.split("/")
+        title = qs_parts[1] if len(qs_parts) > 1 else qs_parts[0]
+        title = None if title.isnumeric() else title
+    return title
+
+
+# Returns True if title is valid, False otherwise
+def title_is_valid(title):
+    return (
+        True
+        if isinstance(title, str) and len(title) >= 6 and len(title) <= 255
+        else False
+    )
+
+
+# Returns a status code method
+def get_status_msg(code):
+    msg = "OK"
+    msg = "Created" if code == 201 else msg
+    msg = "Not Found" if code == 404 else msg
+    msg = "Bad Request" if code == 400 else msg
+    msg = "Internal Server Error" if code == 500 else msg
+    return msg
+
+
+method = get_method()
+id = get_task_id()
+title = get_task_title()
+
+if method == "GET" and not id is None:
+    resp = get_task(id)
+elif method == "GET":
+    resp = get_tasks()
+elif method == "DELETE":
+    resp = delete_task(id)
+elif not title_is_valid(title):
+    resp = ("", 400)
+elif method == "POST":
+    resp = create_task(title)
+elif method == "PUT":
+    resp = update_task(id, title)
+
+print("Status: %d %s" % (resp[1], get_status_msg(resp[1])))
+print("Content-type: application/json\n")
+print(json.dumps(resp[0]))
